99c902be375b17d5a9b93fc4106f69ac14cff759
Cody Poll <cjpoll@gmail.com>
2019-12-11 15:14:42 -0700

    Update router to support Elixir 1.9

--- lib/rivulet/kafka/router.ex
+++ lib/rivulet/kafka/router.ex
@@ -27,6 +27,16 @@ defmodule Rivulet.Kafka.Router do
     Module.put_attribute(__CALLER__.module, :consumer_group, consumer_group)
 
     quote do
+      def child_spec(opts) do
+        %{
+          id: __MODULE__,
+          start: {__MODULE__, :start_link, [opts]},
+          type: :worker,
+          restart: :permanent,
+          shutdown: 500
+        }
+      end
+
       import unquote(__MODULE__)
       alias unquote(__MODULE__)
       @before_compile unquote(__MODULE__)
@@ -34,7 +44,7 @@ defmodule Rivulet.Kafka.Router do
   end
 
   @type ignored :: term
-  @type topic :: String.t
+  @type topic :: String.t()
   @type transformer :: module
 
   defp get_stuff(env) do
@@ -49,7 +59,7 @@ defmodule Rivulet.Kafka.Router do
       end
 
     stream_topics =
-      Enum.map(streams, fn({topic, _routes}) ->
+      Enum.map(streams, fn {topic, _routes} ->
         topic
       end)
 
@@ -61,7 +71,11 @@ defmodule Rivulet.Kafka.Router do
 
     quote do
       def start_link do
-        Rivulet.Kafka.Router.Funcs.start_link(__MODULE__, unquote(consumer_group), unquote(stream_topics))
+        Rivulet.Kafka.Router.Funcs.start_link(
+          __MODULE__,
+          unquote(consumer_group),
+          unquote(stream_topics)
+        )
       end
 
       def init(_) do
@@ -76,28 +90,35 @@ defmodule Rivulet.Kafka.Router do
     end
   end
 
-  defmacro defstream(topic, [do: routes]) do
+  defmacro defstream(topic, do: routes) do
     routes =
       Macro.postwalk(routes, fn
-        ({:transformer, _, [transformer, [do: {:__block__, _, publishes}]]}) ->
+        {:transformer, _, [transformer, [do: {:__block__, _, publishes}]]} ->
           [:route, Macro.expand_once(transformer, __ENV__), publishes]
-        ({:transformer, _, [transformer, [do: publishes]]}) ->
+
+        {:transformer, _, [transformer, [do: publishes]]} ->
           [:route, Macro.expand_once(transformer, __ENV__), publishes]
-        ({:publish_to, _, [topic, config]}) ->
+
+        {:publish_to, _, [topic, config]} ->
           partition_strategy = Keyword.get(config, :partition, :key)
+
           unless partition_strategy in @partition_strategies do
-            raise "#{__CALLER__.module} publishing to #{topic} has an unsupported partition strategy: #{partition_strategy}"
+            raise "#{__CALLER__.module} publishing to #{topic} has an unsupported partition strategy: #{
+                    partition_strategy
+                  }"
           end
 
           {topic, partition_strategy}
-        (node) -> node
+
+        node ->
+          node
       end)
 
     routes =
       case routes do
         {:__block__, [], routes} -> routes
         routes when is_list(routes) -> routes
-        route  -> [route]
+        route -> [route]
       end
 
     set_stream(__CALLER__.module, {topic, routes})
@@ -105,8 +126,8 @@ defmodule Rivulet.Kafka.Router do
     nil
   end
 
-  @spec set_stream(module, {topic, [term]})
-  :: ignored
+  @spec set_stream(module, {topic, [term]}) ::
+          ignored
   defp set_stream(module, stream) do
     Module.put_attribute(module, :streams, stream)
   end
@@ -114,25 +135,33 @@ defmodule Rivulet.Kafka.Router do
   @spec check_duplicate_streams(module) :: ignored
   defp check_duplicate_streams(module) do
     require Logger
+
     case Module.get_attribute(module, :streams) do
       [] ->
         Logger.warn("You didn't define any streams in #{module}. Was this an error?")
+
       streams when is_list(streams) ->
         {_, dups} =
-        Enum.reduce(streams, {[], []}, fn({stream_topic, _}, {topics, dups}) ->
-          if stream_topic in topics do
-            {topics, [stream_topic | dups]}
-          else
-            {[stream_topic | topics], dups}
-          end
-        end)
+          Enum.reduce(streams, {[], []}, fn {stream_topic, _}, {topics, dups} ->
+            if stream_topic in topics do
+              {topics, [stream_topic | dups]}
+            else
+              {[stream_topic | topics], dups}
+            end
+          end)
 
         case dups do
-          [] -> :ok
-          duplicates -> raise "The following stream topics were defined multiple times in #{module}: #{inspect duplicates}"
+          [] ->
+            :ok
+
+          duplicates ->
+            raise "The following stream topics were defined multiple times in #{module}: #{
+                    inspect(duplicates)
+                  }"
         end
 
-      _single_stream -> :ok
+      _single_stream ->
+        :ok
     end
   end
 end
