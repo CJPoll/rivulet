e1d6b48918f0b31ad787d3c03b1bddc27a092492
Cody Poll <cjpoll@gmail.com>
2020-07-20 16:11:48 -0600

    More config fixes

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :rivulet,
   avro_schema_registry_uri: %URI{scheme: "http", host: "rivulet_schema-registry_1", port: 8081},
-  kafka_brokers: [rivulet_kafka_1: 9092],
+  bootstrap_servers: [rivulet_kafka_1: 9092],
   default_client: :"rivulet-client-#{System.get_env("HOSTNAME")}",
   json_handler: Rivulet.JSON.JiffyHandler
