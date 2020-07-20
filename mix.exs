5bb4909d97e3d7104c452bb6e61c3a67e9974da3
Cody Poll <cjpoll@gmail.com>
2020-07-20 16:01:36 -0600

    Update deps

--- mix.exs
+++ mix.exs
@@ -46,12 +46,7 @@ defmodule Rivulet.Mixfile do
   defp deps do
     [
       {:dialyxir, "~> 0.5.0", only: [:dev, :test], runtime: false},
-      {:avro_ex, "~> 0.1.0-beta.5"},
       {:brod, "~> 3.14.0"},
-      {:ecto, "~> 3.0", override: true},
-      {:ecto_sql, "~> 3.0"},
-      {:elastix, "~> 0.5.0"},
-      {:httpoison, ">= 1.0.0"},
       {:meck, "~> 0.8.7", only: [:test]},
       {:jiffy, "0.15.1"},
       {:uuid, "~> 1.1.8"}
